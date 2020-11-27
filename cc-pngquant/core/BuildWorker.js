6c9d77ef2122dd809aa5bf246e1c71d6dc0b1be6
DESKTOP-QM8S1BP\aft <shengxiaainj@gmail.com>
2020-11-27 17:53:24 +0800

    upgrade devDependencies for cc-pngquant

--- cc-pngquant/core/BuildWorker.js
+++ cc-pngquant/core/BuildWorker.js
@@ -10,7 +10,7 @@ const {WorkerBase, registerWorker} = require('./WorkerBase');
 const imagemin = require('imagemin');
 const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
-const Path = require('fire-path');
+// const Path = require('fire-path');
 const Globby = require('globby');
 
 class BuildWorker extends WorkerBase {
@@ -22,38 +22,46 @@ class BuildWorker extends WorkerBase {
 
     imageCompress(dest) {
         dest = dest.replace(/\\/g, '/');
-        let pattern = Path.join(dest, '**/*.{jpg,png}');
-        let paths = Globby.sync(pattern, {});
-        let filesCounts = paths.length;
-        let idx = 0;
-        let newPaths = [];
-        for (let p of paths) {
-            let tmpdest = p;
-            tmpdest = tmpdest.slice(0, tmpdest.lastIndexOf('/'));
-            let last2Dir = tmpdest.slice(tmpdest.lastIndexOf('/') + 1, tmpdest.length);
-            if (last2Dir.match(/\.bundle/g)) {
-            } else {
-                newPaths.push(p);
-            }
-        }
-        filesCounts = newPaths.length;
-        for (let p of newPaths) {
-            let tmpdest = p;
-            tmpdest = tmpdest.slice(0, tmpdest.lastIndexOf('/'));
-            imagemin([p], tmpdest, {
-                plugins: [
-                    imageminJpegtran(),
-                    imageminPngquant({quality: '60-80'})
-                ]
-            }).then(() => {
-                this._updateProgress(1 / filesCounts);
-                idx++;
-                if (idx == filesCounts) {
-                    this._callback && this._callback();
-                    Editor.Ipc.sendToAll('cc-pngquant:state-finished', this._opts);
+        (async () => {
+            const paths = await Globby(dest, {
+                expandDirectories: {
+                    // files: ['*.jpg', '*.png'],
+                    extensions: ['png', 'jpg']
                 }
             });
-        }
+            let filesCounts = paths.length;
+            let idx = 0;
+            let newPaths = [];
+            for (let p of paths) {
+                let tmpDest = p;
+                tmpDest = tmpDest.slice(0, tmpDest.lastIndexOf('/'));
+                let last2Dir = tmpDest.slice(tmpDest.lastIndexOf('/') + 1, tmpDest.length);
+                if (last2Dir.match(/\.bundle/g)) {
+                } else {
+                    newPaths.push(p);
+                }
+            }
+            filesCounts = newPaths.length;
+            for (let p of newPaths) {
+                let tmpDest = p;
+                tmpDest = tmpDest.slice(0, tmpDest.lastIndexOf('/'));
+                imagemin([p], {
+                    destination: tmpDest,
+                    plugins: [
+                        imageminJpegtran(),
+                        imageminPngquant({quality: [0.6, 0.8]})
+                    ]
+                }).then(() => {
+                    this._updateProgress(1 / filesCounts);
+                    idx++;
+                    if (idx === filesCounts) {
+                        this._callback && this._callback();
+                        Editor.Ipc.sendToAll('cc-pngquant:state-finished', this._opts.dest);
+                    }
+                });
+            }
+        })();
+
     }
 }
 
