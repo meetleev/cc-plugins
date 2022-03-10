d2ad5d47f181f4caf37e77a4ba3177e8d6478835
Lee <meetleev@gmail.com>
2022-03-10 11:54:18 +0800

    Delete cc-sdk directory
    move new cc-sdk

--- cc-sdk/lib/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './Log';
-export * from './sdk/EventManager';
-export {resMgr} from './sdk/ResMgr';
-export * from './sdk/AudioMgr';
-export * from './sdk/UIMgr';
-export * from './sdk/ObjectPoolsMgr';
-
-export * from './sdk/utils';
--- cc-sdk/lib/sdk/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './ScreenUtils';
-export * from './PlatformUtils';
